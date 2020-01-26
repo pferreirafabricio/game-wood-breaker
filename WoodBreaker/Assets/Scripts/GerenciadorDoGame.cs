98eba55b35b2fd102a75f3e983c1d3fe56ab22ae
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-01-26 15:41:29 -0200

    Root folder adicinada novamente com o arquivo .gitingore

---
+++ WoodBreaker/Assets/Scripts/GerenciadorDoGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerenciadorDoGame : MonoBehaviour
+{
+    public static void FinalizarJogo()
+    {
+        Application.LoadLevel("CenaPrincipal");
+        //SceneManager.LoadScene("CenaPrincipal");
+    }
+
+}
